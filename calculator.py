f605bf81e341706492e20f43cfbad50166b8af63
Dj_Saha <dhanonjoy6a24@gmail.com>
2022-12-28 11:48:50 +0600

    New code add and create calculator.py file

---
+++ calculator.py
@@ -0,0 +1,16 @@
+num1 = float(input("First number= "))
+operator = input("Enter operator= ")
+num2 = float(input("Second number= "))
+
+if "+" == operator:
+  Add = num1 + num2
+  print(Add)
+elif "-" == operator:
+  Sub = num1 - num2
+  print(Sub)
+elif "*" == operator:
+  Multi = num1 * num2
+  print(Multi)
+elif "/" == operator:
+  Div = num1 / num2
+  print(Div)
--- new_world.py
+++
@@ -1,9 +0,0 @@
-print("Give width")
-W = float(input("="))
-print("Give length")
-L = float(input("="))
-
-Sum = W * L
-print(Sum)
-
-print("Hello \'World\'")
