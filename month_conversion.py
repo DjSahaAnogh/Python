ba481163600abe027bf71bd729dc6fdb21c2702a
Dj_Saha <dhanonjoy6a24@gmail.com>
2022-12-29 22:15:56 +0600

    Create month_conversion.py

---
+++ month_conversion.py
@@ -0,0 +1,16 @@
+monthConversions = {
+  "Jan": "January",
+  "Feb": "February",
+  "Mar": "March",
+  "Apr": "April",
+  "May": "May",
+  "Jun": "June",
+  "Jul": "July",
+  "Aug": "August",
+  "Sep": "September",
+  "Oct": "October",
+  "Nov": "November",
+  "Dec": "December",
+}
+
+print(monthConversions.get("Sep"))
