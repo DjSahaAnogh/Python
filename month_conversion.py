757df5492a09e3f07ce5fc4c6d17af7e20dbe296
Dj_Saha <dhanonjoy6a24@gmail.com>
2022-12-30 18:14:00 +0600

    Update month_conversion.py

--- month_conversion.py
+++ month_conversion.py
@@ -1,17 +1,19 @@
-monthConversions = {
-  "Jan": "January",
-  "Feb": "February",
-  "Mar": "March",
-  "Apr": "April",
-  "May": "May",
-  "Jun": "June",
-  "Jul": "July",
-  "Aug": "August",
-  "Sep": "September",
-  "Oct": "October",
-  "Nov": "November",
-  "Dec": "December",
-}
-
 def month_conversation(keyword):
-  print(monthConversions[keyword])
+  month_conversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+  }
+  print(month_conversions[keyword])
+
+
+month_conversation("Oct")
