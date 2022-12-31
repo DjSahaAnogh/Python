ae5c19a5231f73c9d821bb99f8920c9a017c9ff7
DjSahaAnogh <104628129+DjSahaAnogh@users.noreply.github.com>
2022-12-31 13:20:51 +0600

    update calculator.py

--- calculator.py
+++ calculator.py
@@ -10,18 +10,16 @@ def calculator(num1, num2, operator):
   elif "^2" == operator:
     print(num1 * num1)
   elif "^3" == operator:
-    print(num1*num1*num1)
+    print(num1 * num1 * num1)
   else:
     print("Error")
-    
-
-calculator(10, 5, "-")
 
 
-def raise_power(based_num, pow_num):
+def raise_to_power(base_num, pow_num):
   result = 1
   for index in range(pow_num):
-    result = result * based_num
-    return result
-    
-raise_power(2, 2)
+    result = result * base_num
+  print(result)
+
+
+raise_to_power(10, 10)
