2dc08c15b62d2fe19c988c659cf6beb6f77cdf8a
DjSahaAnogh <104628129+DjSahaAnogh@users.noreply.github.com>
2023-01-13 23:05:30 +0600

    update bill_calculator.py

--- bill_calculator.py
+++ bill_calculator.py
@@ -1,11 +1,17 @@
 # This function will add 5% tip with the bill
-def bill_calculator():
-  amount = int(input("Enter the total amount: "))
-  tip = amount * 5 / 100
-  total_amount = amount + tip
-  print("Tip: ", tip)
-  print("Payable amount: ", total_amount)
+def bill_calculator(in_put):
+  def sum(bill):
+    total = 0
+    for amount in range(0, len(bill)):
+      total = total + bill[amount]
+    print(total)
+  sum(in_put)
+  
+  # tip = amount * 5 / 100
+  # total_amount = amount + tip
+  # print("Tip: ", tip)
+  # print("Payable amount: ", total_amount)
 
 
 # Testing!
-bill_calculator()
+bill_calculator([5, 6, 7])
