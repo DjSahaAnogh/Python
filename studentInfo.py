a0cf1932969fdb7887296f12d93560726d8d6549
DjSahaAnogh <104628129+DjSahaAnogh@users.noreply.github.com>
2023-01-21 21:30:25 +0600

    update studentInfo.py

--- studentInfo.py
+++ studentInfo.py
@@ -1,3 +1,24 @@
-class Add:
-  def __inti__(sefl, num1, num2):
-    self.add = num1 + num2
+class StudentInfo:
+  def __init__(self, id_num, name, grade, roll, sec):
+    self.id_num = id_num
+    self.name = name
+    self.grade = grade
+    self.roll = roll
+    self.sec = sec
+  
+  def check(self):
+    if self.id_num == self.id_num:
+      print(self.name + " " + self.roll + " " + self.sec + " " + self.grade)
+    else:
+      print("Error")
+
+
+student_id_num = input("Student's ID num: ")
+student_name = input("Student's name: ")
+student_roll = input("Student's roll: ")
+student_sec = input("Student's sec: ")
+student_grade = input("Student's grade: ")
+
+student_object = StudentInfo(student_id_num, student_name, student_grade, student_roll, student_sec)
+student_object.check()
+print(student_object)
