ed51fa844b4fe9267d77c3877599d2e9be5f895f
Dhanonjoy Saha Anogh <dhanonjoy6a24@gmail.com>
2023-02-09 23:16:57 +0600

    Answering questions

--- new_world.py
+++ new_world.py
@@ -1,1 +1,12 @@
-print("Hello")
+import sys
+# print("Twinkle, twinkle, little star,")
+# print("	How I wonder what you are!")
+# print("		Up above the world so high,")
+# print("		Like a diamond in the sky. ")
+# print("Twinkle, twinkle, little star, ")
+# print("	How I wonder what you are")
+
+print("Python version")
+print(sys.version)
+print("Version info.")
+print(sys.version_info)
