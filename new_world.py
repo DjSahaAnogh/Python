d64d7b44b1a03ee070e546c798506b1526b1392d
Dhanonjoy Saha Anogh <dhanonjoy6a24@gmail.com>
2023-02-10 22:57:17 +0600

    Anything

--- new_world.py
+++ new_world.py
@@ -1,5 +1,15 @@
-from math import pi
+# from math import pi
+#
+# r = float(input("Enter the radius of the circle: "))
+# ans = pi * r**2
+# print("Answer: "+str("%d" % ans))
 
-r = float(input("Enter the radius of the circle: "))
-ans = pi * r**2
-print("Answer: "+str("%d" % ans))
+first_name = input("First name: ")
+last_name = input("Last name: ")
+print(last_name + " " + first_name)
+
+# values = input("Input some comma separated numbers : ")
+# list_1 = values.split(",")
+# tuple_1 = tuple(list)
+# print("List : ", list)
+# print("Tuple : ", tuple)
