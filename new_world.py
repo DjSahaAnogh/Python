0d429906e2145ad421d850548bc242df8ac03121
Dhanonjoy Saha Anogh <dhanonjoy6a24@gmail.com>
2023-02-11 19:53:57 +0600

    Another question.

--- new_world.py
+++ new_world.py
@@ -1,15 +1,22 @@
-# from math import pi
+import re
 #
 # r = float(input("Enter the radius of the circle: "))
 # ans = pi * r**2
 # print("Answer: "+str("%d" % ans))
 
-first_name = input("First name: ")
-last_name = input("Last name: ")
-print(last_name + " " + first_name)
+# first_name = input("First name: ")
+# last_name = input("Last name: ")
+# print(last_name + " " + first_name)
 
 # values = input("Input some comma separated numbers : ")
 # list_1 = values.split(",")
 # tuple_1 = tuple(list)
 # print("List : ", list)
 # print("Tuple : ", tuple)
+
+string_1 = input("Enter numbers: ")
+numbers = re.findall(r'\d+', string_1)
+list_1 = list(numbers)
+print(list_1)
+tuple_1 = tuple(list_1)
+print(tuple_1)
