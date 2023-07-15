3bfed4e412938b3f9737c342591675476d2f05cd
Dhanonjoy Saha Anogh <dhanonjoy6a24@gmail.com>
2023-07-15 13:44:29 +0600

    New code added

--- resistance.py
+++ resistance.py
@@ -1,24 +1,25 @@
 def resistance():
 	x = input("Enter type: ")
-	if x == ["R", "I", "V"]:
-		return
+	if x in ["R", "I", "V"]:
+		pass
 	else:
 		print("Invalid parameter!")
-	if not x in ["V", "R"] or x == "I":
+		return
+	if not x in ["V", "R"]:
 		V = int(input("Value of Volt: "))
 		R = int(input("Value of Resistance: "))
 
 		I = V / R
 		print(I)
 		return
-	if not x in ["R", "I"] or x == "V":
+	if not x in ["R", "I"]:
 		I = int(input("Value of electricity: "))
 		R = int(input("Value of Resistance: "))
 
 		V = R * I
 		print(V)
 		return
-	if not x in ["I", "V"] or x == "R":
+	if not x in ["I", "V"]:
 		I = int(input("Value of electricity: "))
 		V = int(input("Value of Volt: "))
 
