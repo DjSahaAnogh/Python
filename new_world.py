34167c20d73af9006080156523b462d4bfd27f08
Dhanonjoy Saha Anogh <104628129+DjSahaAnogh@users.noreply.github.com>
2023-12-21 06:56:12 +0000

    Made a rock-paper-scissors game

--- new_world.py
+++ new_world.py
@@ -1,9 +1,32 @@
+import random
+
 def get_choices():
-    player_choice = "Rock"
-    computer_choice = "Paper"
+    player_choice = input("Enter a choice(rock, paper, scissors): ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
 
-    # return player_choice
-    return computer_choice
+def check_win(player, computer):
+    print(f"You chose {player} and the computer chose {computer}.")
+    if player == computer:
+        print("It is a tie.")
+    elif player == "papre":
+        if computer == "rock":
+            print("You win")
+        else:
+            print("You lose")
+    elif player == "rock":
+        if computer == "scissors":
+            print("You win")
+        else:
+            print("You lose")
+    elif player == "scissors":
+        if computer == "paper":
+            print("You win")
+        else:
+            print("You lose")
 
 
-print(get_choices())
+choices = get_choices()
+result = check_win(choices["player"], choices["computer"])
